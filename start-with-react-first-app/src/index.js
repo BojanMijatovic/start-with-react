54bafd5ebc5197b70a5267a579118952de020c4f
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-09 21:40:10 +0200

    create first component

--- start-with-react-first-app/src/index.js
+++ start-with-react-first-app/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
+//   import React and ReactDOM libraries
+import React from 'react';                          // import statements
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//   create React components
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {             // functions base component
+  return <div>Hello there :)</div>
+}
+
+//  take the react component and show it to the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
