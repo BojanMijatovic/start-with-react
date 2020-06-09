4ea7210fb4d0e414d97680193d1cd04897383159
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-09 22:03:38 +0200

    Converting HTML to JSX

--- start-with-react-first-app/src/index.js
+++ start-with-react-first-app/src/index.js
@@ -3,9 +3,14 @@ import React from 'react';                          // import statements
 import ReactDOM from 'react-dom';
 
 //   create React components
-
 const App = () => {             // functions base component
-  return <div>Hello there :)</div>
+  return (                            // convection with multi lines  
+    <div>
+      <label for="name">Name:</label>
+      <input id='name' type='text' />
+      <button>submit</button>
+    </div>
+  );
 }
 
 //  take the react component and show it to the screen
