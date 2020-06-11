1902387c0c7fca0f45365c809bcfaf6f649162a0
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-11 21:55:24 +0200

    Component Reuse

--- start-components/src/CommentDetail.js
+++ start-components/src/CommentDetail.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = (props) => {  //  consume props from parent component
-  console.log(props.author);
+
+
+const CommentDetail = (props) => {  //  consume props argument  from parent component
+  // console.log(props.author);           // props are singe child component 
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatarImg} />
       </a>
       <div className="content">
         <a href="/" className="author">{props.author}</a>
         <div className="metadata">
-          <span className="date">Today at 6 PM</span>
+          <span className="date">{props.date}</span>
         </div>
-        <div className="text">Nice blog post</div>
+        <div className="text">{props.content}</div>
       </div>
+
     </div>
   );
 }
