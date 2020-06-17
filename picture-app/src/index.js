a92768db8a09c08f5b487a47b4c7bf10e112fd89
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-17 20:18:47 +0200

    add search bar

--- picture-app/src/index.js
+++ picture-app/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NewPage from './NewPage';
+import App from './components/App';
 
-class App extends React.Component {
-  state = { name: 'React app' }
 
-  render() {
-    return (
-      <div> <h1>{this.state.name}</h1>
-        <NewPage namePage='Add com' />
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
