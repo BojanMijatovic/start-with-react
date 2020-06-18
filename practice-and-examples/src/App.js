a358d871b41edb82dc8de544a351a384964d0569
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-18 20:47:04 +0200

    good practice

--- practice-and-examples/src/App.js
+++ practice-and-examples/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header'
-
+import MainContent from './components/MainContent'
 
 class App extends React.Component {
 
@@ -8,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <MainContent />
       </div>
     )
   };
---
+++ practice-and-examples/src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import content from '../components/style/content.css'
+
+
+const MainContent = (props) => {
+  return (
+    <p className='main'>This is main content</p>
+  )
+}
+
+
+export default MainContent;
