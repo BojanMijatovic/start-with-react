aa15fdf8032a20907ed6093deccffd47c4dc81d9
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-18 21:44:41 +0200

    start indecision-app

---
+++ indecision-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class IndecisionApp extends React.Component {
+
+  render() {
+    return (
+      <div><h1>App</h1></div>
+    )
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.querySelector('#root'));
