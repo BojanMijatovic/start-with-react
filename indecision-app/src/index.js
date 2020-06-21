892fc1be2dc75a1f66b6e9949ec773b9d893c081
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-21 12:34:10 +0200

    form

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const IndecisionApp = () => {
+class IndecisionApp extends React.Component {
 
-  const template = {
+  template = {
     name: 'Indecision app',
     options: []
   }
 
-  const submitForm = (event) => {
+  submitForm = (event) => {
     event.preventDefault();
+    const option = event.target.elements.option.value;
+    if (option) {
+      this.template.options.push(option);
+      event.target.elements.option.value = '';
+      console.log(this.template.options);
+    }
   }
 
-
-  return (
-    <div>{template.name}
-      <form onSubmit={submitForm}>
-        <label>Input</label>
-        <input type='text' />
-        <button>add option</button>
-      </form>
-    </div>
-  )
+  render() {
+    return (
+      <div>{this.template.name}
+        <div>{this.template.options.length > 0 ? `Here is options: ${this.template.options}` : 'No options'}</div>
+        <form onSubmit={this.submitForm}>
+          <label>Input :</label>
+          <input type='text' name='option' />
+          <button>add option</button>
+        </form>
+        <p>{this.template.options.length}</p>
+      </div>
+    )
+  }
 }
-
 
 ReactDOM.render(<IndecisionApp />, document.querySelector('#root'));
