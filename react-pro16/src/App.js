8e38d9ea047e21b0fae4206b0f123e7f3d17602d
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-25 22:35:40 +0200

    example in Listing

--- react-pro16/src/App.js
+++ react-pro16/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
   state = {
     userName: 'Omega',
     todoItems: [
@@ -13,7 +12,6 @@ class App extends React.Component {
     ],
     newTodoText: ''
   }
-
 
   changeStateData = () => {                        //uses the setState method to set a new value for the userName property.
     this.setState({
@@ -28,16 +26,31 @@ class App extends React.Component {
   }
 
   addNewTodoItem = () => {
-    if (!this.state.todoItems.find(item => item.action === this.state.newTodoText)) {   // if it new todo
+    if (!this.state.todoItems.find(item => item.action === this.state.newTodoText)
+      || this.state.newTodoText.length === ''.trim()) {
       this.setState({
         todoItems: [...this.state.todoItems, { action: this.state.newTodoText, done: false }],  //add new todo
         newTodoText: ''
       })
     }
-    console.log(this.state.todoItems);
-
   }
 
+  toggleTodo = (todo) => {
+    this.setState({
+      todoItems: this.state.todoItems.map(item => {
+        return item.action === todo.action ? { ...item, done: !item.done } : item
+      })
+    })
+  }
+
+  todoTableRows = () => this.state.todoItems.map(item => {
+    return <tr key={item.action}>
+      <td>{item.action}</td>
+      <td>
+        <input type="checkbox" onChange={() => this.toggleTodo(item)} checked={item.done}></input>
+      </td>
+    </tr>
+  })
 
 
   render() {
@@ -47,12 +60,19 @@ class App extends React.Component {
           {this.state.userName} To-do list
         </h4>
         <button className="btn btn-primary m-2" onClick={this.changeStateData}>Change</button>
-        <h3 className="bg-primary text-white text-center">For now we have {this.state.todoItems.filter(item => !item.done).length} todos left</h3>
+        <h3 className="bg-primary text-white text-center">
+          For now we have {this.state.todoItems.filter(item => !item.done).length} todos left</h3>
         <div className="container-fluid">
           <div className="my-1">
             <input className="form-control" type="text" value={this.state.newTodoText} onChange={this.updateNewTodoText} />
             <button className="btn btn-primary mt-1" onClick={this.addNewTodoItem}>add todo</button>
           </div>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr><th>Description</th><th>Done</th></tr>
+            </thead>
+            <tbody>{this.todoTableRows()}</tbody>
+          </table>
         </div>
       </div>
     )
