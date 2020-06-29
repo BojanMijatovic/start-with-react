6580c6fc5e765fea2f047df327fca71782134064
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-06-29 23:02:45 +0200

    add Semantic-UI-React

--- road-to-react/src/App.js
+++ road-to-react/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends React.Component {
 
@@ -10,10 +11,15 @@ class App extends React.Component {
   }
 
   render() {
+    const helloWorld = `Welcome to React road map`;
     return (
-      <div>
-        <h2>Welcome to React road map</h2>
+      <div className='ui container'>
+        <h2 className='ui huge header'>{helloWorld}</h2>
         <h3>This is {this.state.person.name} {this.state.person.lastName}</h3>
+
+        <i className="rs flag"></i>
+        <p>Greets from Serbia</p>
+
       </div>
     )
   }
