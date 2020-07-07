d7c6b3e02c1f9d891a517b47742a6b15209d9af8
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-07-07 21:25:56 +0200

    Extracting JSX to New Components

--- car-demo-app/src/App.js
+++ car-demo-app/src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import faker from 'faker';
+import Car from './components/Car';
 
-import './style/mainApp.css';
 
 class App extends React.Component {
   state = {
-    title: 'Welcome to React'
+    title: 'Welcome to React Car demo app'
   }
 
   render() {
     return (
-      <div>
-        <h1 className='main-title'>{this.state.title}</h1>
-        <div className='car-app'>
-          <label htmlFor='car'>Add Car: </label>
-          <input type='text' placeholder='add car' id='car' />
-          <button>submit</button>
+      <div className='ui container'>
+        <h1 className='ui header'>{this.state.title}</h1>
+        <div className="ui card"  >
+          <div className="image"  >
+            <img src={faker.image.transport()} alt='car' />
+          </div>
+          <div className="content"  >
+            <a href='/http://localhost:3000/' className="header">CHEVROLET</a>
+            <div className="meta" >
+              <span className="date" >year 2015</span>
+            </div>
+            <div className="description" >
+              manufacturer: GENERAL MOTORS
+          </div>
+          </div>
+          <div className="extra content" >
+            <a href='/http://localhost:3000/'>
+              <i className="user icon"></i>"engine":L4, 2.4L; DOHC; 16V</a>
+          </div>
         </div>
+        <Car />
       </div>
     )
   }
---
+++ car-demo-app/src/components/Car.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Car = (props) => {
+  return (
+    <div>
+      <h2>Here is car</h2>
+    </div>
+  )
+}
+
+
+export default Car;
