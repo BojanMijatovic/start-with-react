baf20cdd6dde206c69b7345b765384ec09cd6b21
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-02-18 22:22:12 +0100

    start with babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,3 +1,8 @@
 // console.log('App is running');
 
 // JSX
+const appRoot = document.getElementById('app');
+// const template = < p > This is from JSX < /p>;
+const template = /*#__PURE__*/ React.createElement("p", null, " This is from JSX ")
+
+ReactDOM.render(template, appRoot);
