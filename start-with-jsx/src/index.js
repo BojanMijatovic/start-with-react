072b7d66069c751c6cfc340497b87ee8d9325f9e
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-02-22 21:13:41 +0100

    add

--- start-with-jsx/src/index.js
+++ start-with-jsx/src/index.js
@@ -4,9 +4,20 @@ import ReactDOM from 'react-dom';
 
 //  Create React Component
 const App = () => {
-  return <div><h1>Hello World</h1></div>;
-};
+  return <div> 
+    <label id='name'>Name</label>
+    <input type="text"  id="name"/>
+    <button style={{backgroundColor: 'teal' , color: 'whitesmoke'}}>submit</button>
+    <p> Text here</p>  
+      <p> And  here</p>
 
+  </div>;
+};
+/*
+fokus je bio bas na tome - uzmi i zapisi,
+uradi vezbu, i bez preterivanja, ovo je bila stavka koja me je pokrenula sa
+mrtve tacke razmisljanja i planiranja
+*/ 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
